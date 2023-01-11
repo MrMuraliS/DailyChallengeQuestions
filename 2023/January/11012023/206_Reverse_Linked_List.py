adeb4035aedcc111482cb7b400cd9355f515748f
muralis <muralis1223@gmail.com>
2023-01-11 16:22:55 +0530

    206

---
+++ 2023/January/11012023/206_Reverse_Linked_List.py
@@ -0,0 +1,44 @@
+# 206. Reverse Linked List
+
+"""
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+"""
+
+"""
+xample 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        # Initialize the curr pointer as the head...
+        curr = head
+        # Run a loop till curr points to NULL...
+        # print(head)
+        while curr:
+            # Initialize next pointer as the next pointer of curr...
+            next = curr.next
+            # Now assign the prev pointer to curr’s next pointer.
+            curr.next = prev
+            # Assign curr to prev, next to curr...
+            prev = curr
+            curr = next
+        return prev
