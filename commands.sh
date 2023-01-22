a9cf0da1f29a5e86710b005f8afa382de9b0e714
muralis <muralis1223@gmail.com>
2023-01-22 23:05:53 +0530

    Committing changes on

--- commands.sh
+++ commands.sh
@@ -3,7 +3,7 @@ set -x
 black . --exclude hackerrank.sh &&
 git add . &&
 set +x
-TODAY=$(date +%Y-%m-%d-%H:%M:%S) &&
+TODAY=$(date +%Y-%m-%d, %H:%M:%S) &&
 set -x
 git commit -m "Committing changes on $TODAY"
 wait
