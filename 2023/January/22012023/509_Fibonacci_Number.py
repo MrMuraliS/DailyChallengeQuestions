88e4a230601ed79de2b7bb4ac3c1a838f25bc4ac
muralis <muralis1223@gmail.com>
2023-01-22 20:52:59 +0530

    509

---
+++ 2023/January/22012023/509_Fibonacci_Number.py
@@ -0,0 +1,14 @@
+# 509. Fibonacci Number
+
+"""
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+"""
+
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        return self.fib(n - 1) + self.fib(n - 2)
