7965d2d324c461aa47dee03f2ed666ce13d098b9
muralis <muralis1223@gmail.com>
2023-01-22 22:26:26 +0530

    Committing changes on 2023-01-22-22-26-26

--- hackerrank.sh
+++ hackerrank.sh
@@ -2,6 +2,8 @@ PS4='EXECUTING: '
 set -x
 black . --exclude hackerrank.sh &&
 git add . &&
+set +x
 TODAY=$(date +%Y-%m-%d-%H-%M-%S) &&
+set -x
 git commit -m "Committing changes on $TODAY" &&
 git push
