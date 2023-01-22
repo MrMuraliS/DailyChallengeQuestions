50444cf8eb02374f11d41f10d6e48149b48d1347
muralis <muralis1223@gmail.com>
2023-01-22 22:17:26 +0530

    Hacker Rank Questions on 2023-01-22-22-17-26

--- hackerrank.sh
+++ hackerrank.sh
@@ -1,6 +1,6 @@
 black . --exclude hackerrank.sh &&
 git add . &&
 TODAY=$(date +%Y-%m-%d-%H-%M-%S) &&
-echo "This is sample Test" $TODAY
+echo "This is sample Test $TODAY"
 git commit -m "Hacker Rank Questions on $TODAY" &&
 git push
