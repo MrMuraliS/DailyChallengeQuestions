9dbed0d0b6acd3ab9d16585821b96f180d26f567
muralis <muralis1223@gmail.com>
2023-01-26 09:25:58 +0530

    Committing changes on 2023-01-26-09:25:58

---
+++ 2023/January/26012023/30_Substring_with_Concatenation_of_All_Words.py
@@ -0,0 +1,39 @@
+# 30. Substring with Concatenation of All Words
+
+"""
+You are given a string s and an array of strings words of the same length. Return all starting indices of substring(s) in s that is a concatenation of each word in words exactly once, in any order, and without any intervening characters.
+
+"""
+
+
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        n = len(s)
+        k = len(words)
+        word_length = len(words[0])
+        substring_size = word_length * k
+        word_count = collections.Counter(words)
+
+        def check(i):
+            # Copy the original dictionary to use for this index
+            remaining = word_count.copy()
+            words_used = 0
+
+            # Each iteration will check for a match in words
+            for j in range(i, i + substring_size, word_length):
+                sub = s[j : j + word_length]
+                if remaining[sub] > 0:
+                    remaining[sub] -= 1
+                    words_used += 1
+                else:
+                    break
+
+            # Valid if we used all the words
+            return words_used == k
+
+        answer = []
+        for i in range(n - substring_size + 1):
+            if check(i):
+                answer.append(i)
+
+        return answer
