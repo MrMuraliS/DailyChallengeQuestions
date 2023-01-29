9e4dfa65ba0b91062a443fff7927def22c699494
muralis <muralis1223@gmail.com>
2023-01-29 21:28:37 +0530

    Committing changes on 2023-01-29-21:28:37

--- commands.sh
+++ commands.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 
 PS4="+ ${BASH_SOURCE} : ${LINENO} : "
-black . --exclude hackerrank.sh &&
-git add . &&
-TODAY=$(date +%Y-%m-%d-%H:%M:%S) &&
+black . --exclude hackerrank.sh
+git add .
+TODAY=$(date +%Y-%m-%d-%H:%M:%S)
 git commit -m "Committing changes on $TODAY"
 wait
 git push
