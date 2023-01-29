7e23fc8c8e82036606f161de1a14613aa874a4fb
muralis <muralis1223@gmail.com>
2023-01-29 21:41:09 +0530

    Commiting changes on 2023-01-29-21:41:09

--- commands.sh
+++ commands.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -xe
 PS4="+ ${BASH_SOURCE} : ${LINENO} : "
-black . --exclude hackerrank.sh
-git add .
-TODAY=$(date +%Y-%m-%d-%H:%M:%S)
-git commit -m "Committing changes on $TODAY"
-wait
-git push
+
+function CodePush() {
+    black . --exclude commands.sh
+    git add .
+    local TODAY=$(date +%Y-%m-%d-%H:%M:%S)
+    git commit -m "Commiting changes on ${TODAY}"
+    wait
+    git push
+}
+
+CodePush
