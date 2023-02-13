f3a3640293fd26066cfa07c8ad32f46cd6f04446
muralis <muralis1223@gmail.com>
2023-02-13 21:38:19 +0530

    Commiting changes on 2023-02-13-21:38:19

--- 2023/February/13022023/1529. Minimum Suffix Flips.py
+++ 2023/February/13022023/1529. Minimum Suffix Flips.py
@@ -1,41 +1,37 @@
 # 1529. Minimum Suffix Flips
 
 """
-You are given a binary string nums. You are allowed to perform two types of operations on the string in any sequence:
+You are given a 0-indexed binary string target of length n. You have another binary string s of length n that is initially set to all zeros. You want to make s equal to target.
 
-Type-1: Remove the last character of the string nums and append it to the beginning of the string.
-Type-2: Pick a non-empty prefix of nums and flip every character in that prefix. A flip operation changes a 0 to a 1 and a 1 to a 0.
+In one operation, you can pick an index i where 0 <= i < n and flip all bits in the inclusive range [i, n - 1]. Flip means changing '0' to '1' and '1' to '0'.
 
-Return the minimum number of type-2 operations you need to perform such that nums becomes alternating.
-
-The string is called alternating if no two adjacent characters are equal. For example, the strings "010" and "1010" are alternating, while the string "0100" is not.
+Return the minimum number of operations needed to make s equal to target.
 
 Example 1:
 
-Input: nums = "111000"
-Output: 2
-Explanation: Use the first operation two times to make nums = "100011".
-
-Then, use the second operation on the prefix "1", so nums = "110011".
-
-After the second operation, nums = "101010", which is alternating.
+Input: target = "10111"
+Output: 3
+Explanation: Initially, s = "00000".
+Choose index i = 2: "00000" -> "00111"
+Choose index i = 0: "00111" -> "11000"
+Choose index i = 1: "11000" -> "10111"
+We need at least 3 flip operations to form target.
 
 Example 2:
 
-Input: nums = "010"
-Output: 0
-Explanation: nums is already alternating.
+Input: target = "101"
+Output: 3
+Explanation: "000" -> "111" -> "100" -> "101".
 
 Example 3:
 
-Input: nums = "1110"
-Output: 1
-Explanation: Use the second operation on the prefix "1110" to make nums = "1010".
+Input: target = "00000"
+Output: 0
 
-Constraints:
+Example 4:
 
-1 <= nums.length <= 105
-nums[i] is either '0' or '1'.
+Input: target = "001011101"
+Output: 5
 
 """
 
