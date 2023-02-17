0c66580ea5ac2aedf9d073e020edda26ae752729
muralis <muralis1223@gmail.com>
2023-02-17 23:52:44 +0530

    Commiting changes on 2023-02-17-23:52:44

---
+++ 2023/February/16022023/trying.py
@@ -0,0 +1,14 @@
+# class Solution:
+#     def minValue(self, A: int, B: int, C: int) -> int:
+#         # Write the first step below:
+#         self.A=7
+#         self.B=4
+#         self.C=9
+#         for minval in A:
+#             if B <= minval:
+#                 minval= B
+#             if C <= minval:
+#                 minval= C
+#             return minval
+# res=Solution()
+# print(res.minValue('A', 'B', 'C'))
