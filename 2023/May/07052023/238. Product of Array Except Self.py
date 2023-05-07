5b5fdb6f40c344001aba081ae151d883862c5ae7
muralis <muralis1223@gmail.com>
2023-05-07 06:40:24 +0530

    Commiting changes on 2023-05-07-06:40:24

--- 2023/May/07052023/238. Product of Array Except Self.py
+++ 2023/May/07052023/238. Product of Array Except Self.py
@@ -45,3 +45,16 @@ class Solution:
             left[i] = left[i] * right
             right *= nums[i]
         return left
+
+
+# Solution 3
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        left = [1] * len(nums)
+        for i in range(1, len(nums)):
+            left[i] = left[i - 1] * nums[i - 1]
+        right = 1
+        for i in range(len(nums) - 1, -1, -1):
+            left[i] *= right
+            right *= nums[i]
+        return left
