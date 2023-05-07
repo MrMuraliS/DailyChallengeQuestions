e94b14a4a074fa25a913462511dde9ffcfe83a04
muralis <muralis1223@gmail.com>
2023-05-07 06:37:50 +0530

    Commiting changes on 2023-05-07-06:37:50

---
+++ 2023/May/07052023/238. Product of Array Except Self.py
@@ -0,0 +1,47 @@
+# 238. Product of Array Except Self
+
+"""
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+
+Example 1:
+    Input: nums = [1,2,3,4]
+    Output: [24,12,8,6]
+
+Example 2:
+    Input: nums = [-1,1,0,-3,3]
+    Output: [0,0,9,0,0]
+
+
+Constraints:
+    2 <= nums.length <= 105
+    -30 <= nums[i] <= 30
+    The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+"""
+
+# Solution 1
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        left = [1] * len(nums)
+        right = [1] * len(nums)
+        for i in range(1, len(nums)):
+            left[i] = left[i - 1] * nums[i - 1]
+        for i in range(len(nums) - 2, -1, -1):
+            right[i] = right[i + 1] * nums[i + 1]
+        return [left[i] * right[i] for i in range(len(nums))]
+
+
+# Solution 2
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        left = [1] * len(nums)
+        for i in range(1, len(nums)):
+            left[i] = left[i - 1] * nums[i - 1]
+        right = 1
+        for i in range(len(nums) - 1, -1, -1):
+            left[i] = left[i] * right
+            right *= nums[i]
+        return left
