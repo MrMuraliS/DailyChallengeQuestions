04f4f10010d987ccb0bb5cc83c9a7178f003e446
muralis <muralis1223@gmail.com>
2023-08-11 18:47:44 +0530

    Commiting changes on 2023-08-11-18:47:44

---
+++ 2023/August/10082023/YouTube Recording.py
@@ -0,0 +1,11 @@
+# www.youtube.com/@ClearPyCode
+
+
+# Initialize variables
+total = 0
+count = 0
+
+# Calculate the sum of numbers from 1 to 10
+for i in range(1, 11):
+    total += i
+    count += 1
